6d69087c714085a43cf744cdc354eaeed0986e10
Carol Lewandowski <lewandowski.rod@gmail.com>
2024-07-10 23:55:11 -0300

    fix caminho de imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import './App.css';
+import './styles/App.css';
 import Home from './pages/Home';
 import ContactForm from './components/ContactForm';
 import ContactList from './components/ContactList';
